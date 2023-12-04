7a70359e66b17f6890e683876a02e5525d7fbb40
MathMattar <mathmattar@hotmail.com>
2023-12-04 04:28:16 -0300

    form components and validation logic

---
+++ src/app/components/Form/page.js
@@ -0,0 +1,52 @@
+'use client'
+
+import { Box, Flex } from "@chakra-ui/react";
+import FormInput from "../FormInput/page";
+import { useState } from "react";
+
+export default function Form() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const isFirstNameError = firstName.trim() === '' || !/^[A-Za-z]{3,}$/.test(firstName)
+  const isLastNameError = lastName.trim() === '' || !/^[A-Za-z]{3,}$/.test(lastName)
+  const isEmailError = email.trim() === '' || !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)
+  const isPasswordError = password.trim() === '' || !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)
+
+
+  return (
+    <Box bg='white' p='40px' borderRadius='10px' w='540px'>
+      <Flex flexDir={'column'} rowGap='20px'>
+        <FormInput
+          type="text"
+          placeholder="First Name"
+          error={isFirstNameError}
+          errorMessage="This field must contain at least three letters"
+          onChange={(e) => setFirstName(e.target.value)} />
+
+        <FormInput
+          type="text"
+          placeholder="Last Name"
+          error={isLastNameError}
+          errorMessage="This field must contain at least three letters"
+          onChange={(e) => setLastName(e.target.value)} />
+
+        <FormInput
+          type="email"
+          placeholder="Email"
+          error={isEmailError}
+          errorMessage="Looks like this is not an email"
+          onChange={(e) => setEmail(e.target.value)} />
+
+        <FormInput
+          type="password"
+          placeholder="Password"
+          error={isPasswordError}
+          errorMessage="The password must contain an uppercase and lowercase letter, a number and a special character"
+          onChange={(e) => setPassword(e.target.value)} />
+      </Flex>
+    </Box>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
+import { Box, Flex } from "@chakra-ui/react";
+import Form from "./components/Form/page";
+
 export default function Home() {
   return (
-    <></>
+    <Box w='100%' h='100%'>
+      <Flex flexDir={'row'} wrap={'wrap'} justify={"space-between"} align={'center'} columnGap='45px' >
+        <Box w='525px'>
+          <Flex flexDir={'column'} rowGap='32px'>
+            <h1>Learn to code by watching others</h1>
+            <p>
+              See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
+            </p>
+          </Flex>
+        </Box>
+        <Form />
+      </Flex>
+    </Box>
   )
 }
